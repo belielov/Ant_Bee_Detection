d38379f1baf9ff597c4f98f5a228a77f2830a68a
THice <136684999+belielov@users.noreply.github.com>
2025-04-01 17:47:10 +0800

    Add files via upload

--- src/test_TensorBoard.py
+++ src/test_TensorBoard.py
@@ -2,9 +2,14 @@ import numpy as np
 from torch.utils.tensorboard import SummaryWriter
 from PIL import Image
 
+""" 将图片数据写入TensorBoard日志文件 """
+
 writer = SummaryWriter("logs")
 img_path = "dataset/train/bees/16838648_415acd9e3f.jpg"
+
+# 使用PIL库读取指定路径的图片
 img_PIL = Image.open(img_path)
+
 img_array = np.array(img_PIL)
 print(type(img_array))
 print(img_array.shape)
