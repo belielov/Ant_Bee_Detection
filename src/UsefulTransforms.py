348674ace71ece3df2d4970296a789767a4ff57d
THice <136684999+belielov@users.noreply.github.com>
2025-04-01 22:11:17 +0800

    Add files via upload

--- src/UsefulTransforms.py
+++ src/UsefulTransforms.py
@@ -1,9 +1,14 @@
-from PIL import Image
-from torch.utils.tensorboard import SummaryWriter
-from torchvision import transforms
+from PIL import Image # 处理图像
+from torch.utils.tensorboard import SummaryWriter # 记录数据到TensorBoard
+from torchvision import transforms # 图像预处理工具
 
+# 创建SummaryWriter对象，指定日志保存路径为 "logs"
 writer = SummaryWriter("logs")
+
+# 定义图像路径
 img = Image.open("dataset/train/ants/0013035.jpg")
+
+# 打印图片信息（格式、尺寸等）
 print(img)
 
 # ToTensor
@@ -21,15 +26,13 @@ writer.add_image("Normalize", img_norm, 2)
 # Resize
 print(img.size)
 trans_resize = transforms.Resize((512,512))
-# img PIL -> resize -> img_resize PIL
-img_resize = trans_resize(img)
-# img_resize PIL -> totensor -> img_resize tensor
-img_resize = trans_totensor(img_resize)
+img_resize = trans_resize(img)          # img PIL -> resize -> img_resize PIL
+img_resize = trans_totensor(img_resize) # img_resize PIL -> totensor -> img_resize tensor
 writer.add_image("Resize", img_resize, 0)
 print(img_resize)
 
 # Compose - resize -2
-trans_resize_2 = transforms.Resize(512)
+trans_resize_2 = transforms.Resize(512) # 定义Resize转换（将短边缩放到512，长边按比例调整）
 trans_compose = transforms.Compose([trans_resize_2, trans_totensor])
 img_resize_2 = trans_compose(img)
 writer.add_image("Resize", img_resize_2, 1)
@@ -41,4 +44,5 @@ for i in range(10):
     img_crop = trans_compose_2(img)
     writer.add_image("RandomCrop", img_crop, i)
 
+# 关闭写入器（确保数据写入磁盘）
 writer.close()
